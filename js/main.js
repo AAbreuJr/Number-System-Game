2f6f28e0189b79be8a92387dc1e1040bf7448479
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-08-12 09:37:42 -0400

    change javascript around

--- js/main.js
+++ js/main.js
@@ -1,88 +1,24 @@
 /*------Constants------*/
 
-// CHORDS FOR EACH KEY
-const chordsOfKey = [
-{
-    root: "A",
-    // chords: ["A", "Bm", "C#m", "D", "E", "F#m", "G#"],    
-},
-{
-    root: "B",
-    // chords: ["B", "C#m", "D#m", "E", "F#", "G#m", "A#"],
-},
-{
-    root: "C",
-    // chords: ["C", "Dm", "Em", "F", "G", "Am", "B"],
-},
-{
-    root: "D",
-    // chords: ["D", "Em", "F#m", "G#m", "G", "Bm", "C#"],
-},
-{
-    root: "E",
-    // chords: ["E", "F#m", "G#m", "A", "B", "C#m", "D#"],
-},
-{
-    root: "F",
-    // chords: ["F", "Gm", "Am", "Bb", "C", "Dm", "E"],
-},
-{
-    root: "G",
-    // chords: ["G", "Am", "Bm", "C", "D", "Em", "F#"],
-},
-]
+// Array of keys
+const keysArray = ["A", "B", "C", "D", "E", "F", "G"]
 
 //PROGRESSIONS
 const progressionArray = [
 {
     progression:["1", "5", "6", "4"],
-    answerForA: ["A", "E", "F#m", "D"],
-    answerForB: ["B", "F#", "G#m", "E"],
-    answerForC: ["C", "G", "Am", "F"],
-    answerForD: ["D", "A", "Bm", "G"],
-    answerForE: ["E", "B", "C#m", "A"],
-    answerForF: ["F", "C", "Dm", "Bb"],
-    answerForG: ["G", "D", "Em", "C"],
 },
 {
-    progression: ["6", "2", "5", "1"],
-    answerForA: ["F#m", "Bm", "E", "A"],
-    answerForB: ["G#m", "C#m", "F#", "B"],
-    answerForC: ["Am", "Dm", "G", "C"],
-    answerForD: ["Bm", "Em", "A", "D"],
-    answerForE: ["C#m", "F#m", "B", "E"],
-    answerForF: ["Dm", "Gm", "C", "F"],
-    answerForG: ["Em", "Am", "D", "G"],
+    progression: ["6", "2", "5", "1"], 
 },
 {
     progression: ["6", "4", "1", "5"],
-    answerForA: ["F#m", "D", "A", "E"],
-    answerForB: ["G#m", "E", "B", "F#"],
-    answerForC: ["Am", "F", "C", "G"],
-    answerForD: ["Bm", "G", "D", "A"],
-    answerForE: ["C#m", "A", "E", "B"],
-    answerForF: ["Dm", "Bb", "F", "C"],
-    answerForG: ["Em", "C", "G", "D"],
 },
 {
     progression: ["1", "4", "6", "5"],
-    answerForA: ["A", "D", "F#M", "E"],
-    answerForB: ["B", "E", "G#m", "F#"],
-    answerForC: ["C", "F", "Am", "G"],
-    answerForD: ["D", "G", "Bm", "A"],
-    answerForE: ["E", "A", "C#m", "B"],
-    answerForF: ["F", "Bb", "Dm", "C"],
-    answerForG: ["G", "C", "Em", "D"],
 },
 {
     progression: ["1", "3", "6", "4"],
-    answerForA: ["A", "C#m", "F#m", "D"],
-    answerForB: ["B", "D#m", "G#m", "E"],
-    answerForC: ["C", "Em", "Am", "F"],
-    answerForD: ["D", "F#m", "Bm", "G"],
-    answerForE: ["E", "G#m", "C#m", "A"],
-    answerForF: ["F", "Am", "Dm", "Bb"],
-    answerForG: ["G", "Bm", "Em", "C"],
 }
 ];
 
@@ -128,8 +64,7 @@ function start(){
     init();
 }
 
-// Initialization function:
-// Where you set your initial state, setting up 
+// Initialization function: 
 // what the screen will look like upon loading
 function init(){
     getRandomKey();
@@ -142,8 +77,8 @@ function init(){
 
 // gets a random root note to show up
 function getRandomKey (evt){
-    let randomKey = chordsOfKey[Math.floor(Math.random() * chordsOfKey.length)]
-    document.getElementById('newKey').innerHTML = `${randomKey.root}`;
+    let randomKey = keysArray[Math.floor(Math.random() * keysArray.length)]
+    document.getElementById('newKey').innerHTML = `${randomKey}`;
     return {randomKey};  
 }
 
@@ -157,9 +92,11 @@ function getRandomProg (evt){
 
 // Check winner function:
 // Checks the current state of the board for
-function game(){}
+
 
 // Render function:
 // Displays the current state of the board
 // on the page, updating the elements to reflect
 // either X or O depending on whose turn it is
+
+
---
+++ js/test.js
@@ -0,0 +1,76 @@
+// CHORDS FOR EACH KEY
+// const chordsOfKey = [
+//     {
+//         root: "A",
+//         // chords: ["A", "Bm", "C#m", "D", "E", "F#m", "G#"],    
+//     },
+//     {
+//         root: "B",
+//         // chords: ["B", "C#m", "D#m", "E", "F#", "G#m", "A#"],
+//     },
+//     {
+//         root: "C",
+//         // chords: ["C", "Dm", "Em", "F", "G", "Am", "B"],
+//     },
+//     {
+//         root: "D",
+//         // chords: ["D", "Em", "F#m", "G#m", "G", "Bm", "C#"],
+//     },
+//     {
+//         root: "E",
+//         // chords: ["E", "F#m", "G#m", "A", "B", "C#m", "D#"],
+//     },
+//     {
+//         root: "F",
+//         // chords: ["F", "Gm", "Am", "Bb", "C", "Dm", "E"],
+//     },
+//     {
+//         root: "G",
+//         // chords: ["G", "Am", "Bm", "C", "D", "Em", "F#"],
+//     },
+    ]
+
+    //1564
+    // answerForA: ["A", "E", "F#m", "D"],
+    // answerForB: ["B", "F#", "G#m", "E"],
+    // answerForC: ["C", "G", "Am", "F"],
+    // answerForD: ["D", "A", "Bm", "G"],
+    // answerForE: ["E", "B", "C#m", "A"],
+    // answerForF: ["F", "C", "Dm", "Bb"],
+    // answerForG: ["G", "D", "Em", "C"],
+
+    //6251
+    // answerForA: ["F#m", "Bm", "E", "A"],
+    // answerForB: ["G#m", "C#m", "F#", "B"],
+    // answerForC: ["Am", "Dm", "G", "C"],
+    // answerForD: ["Bm", "Em", "A", "D"],
+    // answerForE: ["C#m", "F#m", "B", "E"],
+    // answerForF: ["Dm", "Gm", "C", "F"],
+    // answerForG: ["Em", "Am", "D", "G"],
+
+    //6415
+    // answerForA: ["F#m", "D", "A", "E"],
+    // answerForB: ["G#m", "E", "B", "F#"],
+    // answerForC: ["Am", "F", "C", "G"],
+    // answerForD: ["Bm", "G", "D", "A"],
+    // answerForE: ["C#m", "A", "E", "B"],
+    // answerForF: ["Dm", "Bb", "F", "C"],
+    // answerForG: ["Em", "C", "G", "D"],
+
+    //1465
+    // answerForA: ["A", "D", "F#M", "E"],
+    // answerForB: ["B", "E", "G#m", "F#"],
+    // answerForC: ["C", "F", "Am", "G"],
+    // answerForD: ["D", "G", "Bm", "A"],
+    // answerForE: ["E", "A", "C#m", "B"],
+    // answerForF: ["F", "Bb", "Dm", "C"],
+    // answerForG: ["G", "C", "Em", "D"],
+
+    //1364
+    // answerForA: ["A", "C#m", "F#m", "D"],
+    // answerForB: ["B", "D#m", "G#m", "E"],
+    // answerForC: ["C", "Em", "Am", "F"],
+    // answerForD: ["D", "F#m", "Bm", "G"],
+    // answerForE: ["E", "G#m", "C#m", "A"],
+    // answerForF: ["F", "Am", "Dm", "Bb"],
+    // answerForG: ["G", "Bm", "Em", "C"],
