71ef1af563fd2bd66cec8a675b9e1a153236116e
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-08-12 11:26:38 -0400

    figure out win logic

--- js/main.js
+++ js/main.js
@@ -30,6 +30,8 @@ let randomKey = [];
 let chords = [];
 let randomProg = [];
 let input;
+let winner;
+let tryAgain;
 
 /*------Cached Element References------*/
 
@@ -37,10 +39,10 @@ let input;
 const resetButton = document.getElementById('resetBtn');
 const submitButton = document.getElementById('submitBtn');
 const gameCheck = document.getElementById('rightOrWrong');
-let firstAns = document.getElementById('firstAnswer');
-let secondAns = document.getElementById('secondAnswer');
-let thirdAns = document.getElementById('thirdAnswer');
-let fourthAns = document.getElementById('fourthAnswer');
+const firstAns = document.getElementById('userInput1').value;
+const secondAns = document.getElementById('userInput2').value;
+const thirdAns = document.getElementById('userInput3').value;
+const fourthAns = document.getElementById('userInput4').value;
 
 /*------Event Listeners------*/
 
@@ -49,7 +51,7 @@ let fourthAns = document.getElementById('fourthAnswer');
 
 // document.getElementById('startScreen').addEventListener('click', start);
 document.getElementById('resetBtn').addEventListener('click', reset);
-// document.getElementById('submitBtn').addEventListener('click', chordCheck);
+document.getElementById('submitBtn').addEventListener('click', isRight);
 
 /*------Functions------*/
 init();
@@ -59,20 +61,15 @@ function reset(){
     init();
 }
 
-//start screen
-function start(){
-    init();
-}
-
 // Initialization function: 
 // what the screen will look like upon loading
 function init(){
     getRandomKey();
     getRandomProg();
-    firstAns.value = '';
-    secondAns.value = '';
-    thirdAns.value = '';
-    fourthAns.value = '';
+    userInput1.value = '';
+    userInput2.value = '';
+    userInput3.value = '';
+    userInput4.value = '';
 }
 
 // gets a random root note to show up
@@ -92,11 +89,52 @@ function getRandomProg (evt){
 
 // Check winner function:
 // Checks the current state of the board for
+function isRight(){
+    //keyofA progression 1 | 1-5-6-4
+    if (randomProg === progressionArray[0] 
+    && userInput1.value === "A" 
+    && userInput2.value === "E" 
+    && userInput3.value === "F#m" || "Gbm" 
+    && userInput4.value === "D") {
+        console.log (`Nice One`)
+    
+    
+   //key of A progression 2 | 6-2-5-1
+    } else if (randomProg === progressionArray[1] 
+    && userInput1.value === "F#m" || "Gbm" 
+    && userInput2.value === "Bm" 
+    && userInput3.value === "E" 
+    && userInput4.value === "A") {
+        console.log (`Nice One 2`)
+         
+   //key of A progression 3 | 6-4-1-5
+    } else if (randomProg === progressionArray[2]
+    && userInput1.value === "F#m" || "Gbm"
+    && userInput2.value === "Bm"
+    && userInput3.value === "E"
+    && userInput4.value === "A") {
+        console.log (`Nice One 3`)
+    
+    // key of A progression 4 | 1-4-6-5
+    } else if (randomProg === progressionArray[3]
+    && userInput1.value === "A"
+    && userInput2.value === "D"
+    && userInput3.value === "F#m"
+    && userInput4.value === "E") {
+        console.log (`Nice One 4`)
 
+    // key of A progression 5 | 
+    } else if (randomProg === progressionArray[3]
+        && userInput1.value === "A"
+        && userInput2.value === "D"
+        && userInput3.value === "F#m"
+        && userInput4.value === "E") {
+            console.log (`Nice One 4`)
+}
+}
 
 // Render function:
 // Displays the current state of the board
 // on the page, updating the elements to reflect
 // either X or O depending on whose turn it is
-
 
